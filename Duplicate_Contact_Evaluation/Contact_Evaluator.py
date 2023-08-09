9d9b4a98c690c707c041bfd49d6237cb792357d7
Avry Luy <avryluy@mailbox.org>
2023-08-09 14:59:21 -0500

    Updated code based on Black formatter

--- Duplicate_Contact_Evaluation/Contact_Evaluator.py
+++ Duplicate_Contact_Evaluation/Contact_Evaluator.py
@@ -19,8 +19,10 @@ def get_date():
 
 def conditions(df):
     points = 0
-    if df["EmCount"] == df["MatchTitle"]: points += 1
-    else: points += 0
+    if df["EmCount"] == df["MatchTitle"]: 
+        points += 1
+    else:
+        points += 0
     if (df["Account_Status__c"] == "Inactive"): points += 1
     else: points += 0 
     if df["Title"] != "N/A": points += 1
@@ -123,17 +125,24 @@ print("Data Cleaned. Took {:.2f} seconds".format(time_clean_delta))
 
 time_help_start = time.time()
 #Column to match if account name is in contact email domain
-df["A_Name_Match_C_Em_Dom"] = df.apply(lambda x: x.AccountName.replace(" ","").lower() in x.contactemaildomain, axis=1, result_type="expand").to_frame()
+df["A_Name_Match_C_Em_Dom"] = df.apply(lambda x: x.AccountName.replace(" ","").lower()
+                                        in x.contactemaildomain, axis=1, 
+                                        result_type="expand").to_frame()
 #Max Date
 df["MaxDate"] = df.groupby("email")["CreatedDate"].transform(lambda x :x.max())
 df["EmDomInAcName"] = df.apply(lambda x: x.em_dom in x.AccountName.lower(), axis=1)
 df["AcNameInEmDom"] = df.apply(lambda x: x.AccountName.lower() in x.em_dom, axis=1)
 #COUNT(*) OVER () WINDOW FUNCTION
-df["MatchLnFn"] = df.groupby(["email","LastName","FirstName"])["contactid"].transform(lambda x : x.size)
-df["MatchLN"] = df.groupby(["email", "LastName"])["contactid"].transform(lambda x :x.size)
-df["MatchLn3Fn"] = df.groupby(["email","LastName","First3Name"])["contactid"].transform(lambda x :x.size)
-df["MatchTitle"] = df.groupby(["email","Title"])["contactid"].transform(lambda x: x.size)
-df["MatchPhone"] = df.groupby(["email","ContactPhone"])["contactid"].transform(lambda x: x.size)
+df["MatchLnFn"] = df.groupby(["email","LastName","FirstName"])["contactid"].transform(
+    lambda x : x.size)
+df["MatchLN"] = df.groupby(["email", "LastName"])["contactid"].transform(
+    lambda x :x.size)
+df["MatchLn3Fn"] = df.groupby(["email","LastName","First3Name"])["contactid"].transform(
+    lambda x :x.size)
+df["MatchTitle"] = df.groupby(["email","Title"])["contactid"].transform(
+    lambda x: x.size)
+df["MatchPhone"] = df.groupby(["email","ContactPhone"])["contactid"].transform(
+    lambda x: x.size)
 time_help_end = time.time()
 time_help_delta =  time_help_end - time_help_start
 
@@ -141,9 +150,12 @@ print("Helper Columns Created. Took {:.2f} seconds.".format(time_help_delta))
 
 time_eval_start = time.time()
 dupeEval = [
-df["EmCount"].eq(2) & (df["MatchLnFn"].eq(1) & df["MatchLN"].eq(1) & df["MatchLn3Fn"].eq(1)),
-df["EmCount"].eq(3) & (df["MatchLnFn"].eq(1) & df["MatchLN"].eq(1) & df["MatchLn3Fn"].eq(1)),
-df["EmCount"].gt(df["MatchLnFn"]) & df["EmCount"].gt(df["MatchLn3Fn"]) & df["EmCount"].gt(df["MatchLN"]),
+df["EmCount"].eq(2) & (df["MatchLnFn"].eq(1) & df["MatchLN"].eq(1) & 
+                       df["MatchLn3Fn"].eq(1)),
+df["EmCount"].eq(3) & (df["MatchLnFn"].eq(1) & df["MatchLN"].eq(1) & 
+                       df["MatchLn3Fn"].eq(1)),
+df["EmCount"].gt(df["MatchLnFn"]) & df["EmCount"].gt(
+    df["MatchLn3Fn"]) & df["EmCount"].gt(df["MatchLN"]),
 df["MatchLn3Fn"].gt(df["MatchLnFn"]) | df["MatchLn3Fn"].gt(df["MatchLN"]),
 df["MatchLN"].gt(df["MatchLnFn"]) | df["MatchLN"].gt(df["MatchLn3Fn"]),
 df["EmCount"].eq(df["MatchLn3Fn"])
@@ -164,16 +176,23 @@ df["points2"] = df.apply(conditions_phase2, axis=1)
 df["points_total"] = df["points"] + df["points2"]
 
 df.sort_values(["EmCount","email","points"], inplace=True)
-df["Rank"] = df.groupby("email")["points_total"].rank(ascending=False,method="average").to_frame()
+df["Rank"] = df.groupby("email")["points_total"].rank(
+    ascending=False,method="average").to_frame()
 
 
-df["eval_result"] = df.groupby("email")["points_total"].transform(lambda x: x.sum()/df["EmCount"])
-df["Review_Groups"] = df["email"].map(df.groupby("email").apply(lambda x: x["Rank"].ne(1).all())) #Returns True if all rows in an email group that does not have a Rank of 1
-df["Merge_Groups"] = df["email"].map(df.groupby("email").apply(lambda x: x["Rank"].eq(1).any())) #Returns True if any row in an email group has a Rank of 1
+df["eval_result"] = df.groupby("email")["points_total"].transform(lambda x: x.sum()/
+                                                                  df["EmCount"])
+df["Review_Groups"] = df["email"].map(df.groupby("email")
+                                      .apply(lambda x: x["Rank"].ne(1).all())) 
+#Returns True if all rows in an email group that does not have a Rank of 1
+df["Merge_Groups"] = df["email"].map(df.groupby("email")
+                                     .apply(lambda x: x["Rank"].eq(1).any()))
+ #Returns True if any row in an email group has a Rank of 1
 master_conditions = [
     df["Rank"].eq(1) & df["eval_result"].ne(df["points_total"]),
     df["Rank"].ne(1) & df["Merge_Groups"].eq(True),
-    df["eval_result"].eq(df["points_total"]) | (df["Rank"] % 1).ne(0) | df["Review_Groups"].eq(True)  
+    df["eval_result"].eq(df["points_total"]) | (df["Rank"] % 1).ne(0)
+      | df["Review_Groups"].eq(True)  
 ]
 
 master_choices = ["Primary","Merge","Review"]
@@ -197,13 +216,20 @@ df["Missing Master?"] = ""
 
 df.drop(['First3Name','em_dom','am_dom','ae_dom','A_Upper','A_Name_Match_C_Em_Dom',
 'MaxDate','EmDomInAcName','AcNameInEmDom','MatchLnFn','MatchLN','MatchLn3Fn','MatchTitle',
-'MatchPhone','points','points2','eval_result',"Review_Groups","Merge_Groups"],axis = 1,inplace=True)
+'MatchPhone','points','points2','eval_result',"Review_Groups","Merge_Groups"],
+axis = 1,inplace=True)
 
 
-df  = df.reindex(columns=['Data Load Status','Data Load Date','Dupes','points_total','Rank','MasterPicker','Notes','Researcher','Research Status','Research Date','Not a Duplicate?',
-'Master Record?','Include?','Missing Master?','OppCount','Type','email','contactemaildomain','AccountName','ParentName','UltParentName','accntwebdomain','accntemaildomain','ContactName',
-'ContactPhone','AccountPhone','Account_Status__c','Contact_Status__c','CreatedDate','UsingPartnerCommunity','ContactZipCode','AccountNumber','AccountZipCode','UPSameAsAccount','EmCount',
-'FirstName','LastName','Title','accountid','ParentID','UltParentID','contactid'])
+df  = df.reindex(columns=['Data Load Status','Data Load Date','Dupes','points_total',
+                        'Rank','MasterPicker','Notes','Researcher','Research Status',
+                        'Research Date','Not a Duplicate?','Master Record?','Include?',
+                        'Missing Master?','OppCount','Type','email',
+                        'contactemaildomain','AccountName','ParentName','UltParentName',
+                        'accntwebdomain','accntemaildomain','ContactName','ContactPhone',
+                        'AccountPhone','Account_Status__c','Contact_Status__c','CreatedDate',
+                        'UsingPartnerCommunity','ContactZipCode','AccountNumber','AccountZipCode',
+                        'UPSameAsAccount','EmCount','FirstName','LastName','Title',
+                        'accountid','ParentID','UltParentID','contactid'])
 
 
 path = (r"~\Desktop\ContactExports")
@@ -212,6 +238,8 @@ time_export_end = time.time()
 time_export_delta = time_export_end - time_export_start
 print("Export Completed. Took {:.2f} seconds.".format(time_export_delta))
 
-time_total = time_connect_delta + time_clean_delta + time_eval_delta + time_export_delta 
+time_total = time_connect_delta + time_clean_delta 
++ time_eval_delta + time_export_delta 
+
 time.sleep(1)
 print("Total time: {:.2f} seconds.".format(time_total))
